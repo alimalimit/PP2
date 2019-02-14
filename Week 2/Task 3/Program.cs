b1383857ebef52992801db5c49ea6c1729e80bbf
Alima <alimalimit@gmail.com>
2019-02-15 03:04:11 +0600

    commit

---
+++ Week 2/Task 3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        //создадим рекурсивную функцию, которая поможет визуально изобразить
+        //местоположение файлов или папок, используя отступы
+        static void PrintInfo(FileSystemInfo d, int n)
+        {
+            string stair = new string(' ', n); // создаем строку состоящая из пробела, повторяющегося n раз
+            stair = stair + d.Name; // пусть наш файл или папка будут иметь отступ, чтобы 
+            //показать раположение папки или файла
+            Console.WriteLine(stair);
+
+
+            //если мы работаем с папкой, то тогда приводим нашу переменную FileSystemInfo d к типу DirectoryInfo
+            //а затем извлекаем из нее содержимое
+            if (d.GetType() == typeof(DirectoryInfo)) 
+            {
+                var folders = (d as DirectoryInfo).GetFileSystemInfos();
+                //для содержимого папки создаем еще больший отступ, заново вызывая функцию
+                foreach ( var t in folders)
+                {
+                    PrintInfo(t, n + 5);
+                }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Admin\Desktop\Alima Eldes\study");
+            PrintInfo(dir, 0);//передаем в функцию нужную папку и пусть начальные папки и файлы будут без отступа
+        }
+
+        
+    }
+}
---
+++ Week 2/Task 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string oldPath = @"C:\Users\Admin\Desktop\физика\testing\Old";
+            string newPath = @"C:\Users\Admin\Desktop\физика\testing\New";
+            
+
+            string fileName = "hello world";
+
+            string sourcefile = Path.Combine(oldPath, fileName);
+            string targetfile = Path.Combine(newPath, fileName);
+
+            using (File.Create(sourcefile))
+            {
+
+            };
+
+            File.Copy( sourcefile,targetfile , true);
+
+            File.Delete(Path.Combine(oldPath, fileName));
+
+        }
+           
+        }
+    }
+
